c33fee8f8e5afe0e733d366bb647ff8fdc6a0ef1
hakbum <hhbhong@gmail.com>
2020-04-06 15:12:52 +0900

    setupProxy 수정

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,13 +1,13 @@
 const { createProxyMiddleware } = require('http-proxy-middleware');
 
 module.exports = function (app) {
-  app.use('/', createProxyMiddleware({
+  app.use(createProxyMiddleware('/kobis',{
     target: 'http://www.kobis.or.kr',
     changeOrigin: true,
   }));
-  app.use('/', createProxyMiddleware({
+  app.use(createProxyMiddleware('/themoviedb',{
     target: 'https://api.themoviedb.org',
     changeOrigin: true,
-  }));
+  }))
 };
 
